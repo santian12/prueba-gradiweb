8bbf4df0a5672afb104c453d174bec7c90473e68
Santiago <santiagoanmucha@gmail.com>
2023-09-02 14:57:27 -0500

    sistema de estrellas realizado

--- functions.js
+++ functions.js
@@ -1,14 +1,78 @@
 const url = "https://gradistore-spi.herokuapp.com/products/all";
-const xhr = new XMLHttpRequest();
+const responseDiv = document.getElementById("response");
+document.addEventListener("DOMContentLoaded", function() {
+    const responseDiv = document.getElementById("response");
+  
+    if (responseDiv) {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data); // Imprime la respuesta completa en la consola para inspección
+  
+          if (Array.isArray(data.products.nodes) && data.products.nodes.length > 0) {
+            const products = data.products.nodes;
+            const productHTML = products.map((product) => {
+              return `
+              <div class="product-card">
+              <div class="product-image">
+                  <span class="discount-tag">50% off</span>
+                  <img src="${product.featuredImage.url}" class="product-thumb" alt="">
+                  <button class="card-btn">add to wishlist</button>
+              </div>
+              <div class="product-info">
+                  <h2 class="product-brand">${product.title}</h2>
+                  <p class="product-short-description">${product.tags}</p>
+                  <span class="price">${product.prices.max.amount} ${product.prices.max.currencyCode}</span><span class="actual-price"></span>
 
-xhr.addEventListener('load', function() {
-  if (this.readyState === 4 && this.status === 200) {
-  const productos= JSON.parse(this.response)
-  console.log(productos)
-  } else {
-    console.log("Error: " + this.statusText);
-  }
-});
+              </div>
+          </div>
+              `;
+            }).join('');
+  
+            responseDiv.innerHTML = productHTML;
+          } else {
+            console.error("No se encontraron elementos para mostrar.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else {
+      console.error("El elemento con el ID 'response' no se encontró en el documento.");
+    }
+  });
+  
+  // Styles slider
+ // Inicializa el carrusel 
 
-xhr.open('GET', url);
-xhr.send();
+ document.addEventListener("DOMContentLoaded", function () {
+    const productContainers = document.querySelectorAll(".product-container");
+    const nxtBtns = document.querySelectorAll(".nxt-btn");
+    const preBtns = document.querySelectorAll(".pre-btn");
+  
+    function handleScroll(container, scrollAmount) {
+      container.scrollLeft += scrollAmount;
+    }
+  
+    nxtBtns.forEach((nxtBtn) => {
+      nxtBtn.addEventListener("click", () => {
+        const container = nxtBtn.closest(".product-container");
+        if (container) {
+          handleScroll(container, container.offsetWidth);
+        }
+      });
+    });
+  
+    preBtns.forEach((preBtn) => {
+      preBtn.addEventListener("click", () => {
+        const container = preBtn.closest(".product-container");
+        if (container) {
+          handleScroll(container, -container.offsetWidth);
+        }
+      });
+    });
+  });
+
+ 
+  
+  
